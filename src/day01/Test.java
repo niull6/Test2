c6bc6ca719bc67e8b937c2fd5fec1d613effde52
niu <niulonglong1995@163.com>
2019-11-17 21:21:55 +0800

    修改了Test.class的代码

--- src/day01/Test.java
+++ src/day01/Test.java
@@ -7,5 +7,9 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("你好啊");
+        for(int i=1;i<10;i++){
+            System.out.println(i);
+        }
     }
 }
